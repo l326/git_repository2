d5b17b98c54e5d11044f67a152d33eaa658008f2
l326 <3458262441@qq.com>
2020-05-13 17:34:36 +0800

    提交

--- test/src/main/java/com/usian/java/Test1.java
+++ test/src/main/java/com/usian/java/Test1.java
@@ -3,5 +3,8 @@ package com.usian.java;
 public class Test1 {
         public static void main(String[] args) {
             System.out.println("世界你好");
+            /**
+             * 本地idea
+             */
         }
 }
